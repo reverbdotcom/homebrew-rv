4e0f96bb2730727a1e9f205ba606520966859aaa
Erik Benoist <ebenoist@gmail.com>
2016-02-06 11:58:03 -0600

    Bumps to v0.0.3

--- rv.rb
+++ rv.rb
@@ -1,10 +1,10 @@
 class Rv < Formula
-  @version = "v0.0.2"
+  @version = "v0.0.3"
 
   desc "AWS node resolver"
   homepage "https://github.com/ebenoist/rv"
   url "https://github.com/ebenoist/rv/releases/download/#{@version}/binaries.tar.gz"
-  sha256 "4d6563dd902fd47d63b9377846c3db84a1565e1ce1f3a74b19edd7f2fcc70d29"
+  sha256 "2c6990604258ebfcaa405b45d611fd37b907de700534ba5ef82ad1f6a189a537"
   version @version
 
   def install
