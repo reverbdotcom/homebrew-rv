44aa1433823f90d7ef331a7a957d5c93287d6557
Erik Benoist <ebenoist@gmail.com>
2016-02-09 21:22:01 -0600

    Bumping to v0.0.4

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: release
+
+release:
+	@echo "Building version $(VERSION)"
+	@curl -s https://github.com/ebenoist/rv/releases/download/$(VERSION)/binaries.tar.gz | shasum -a 256 | awk '{ print $$1 }' > SHA
+	@echo "SHA is `cat SHA`"
+	@echo $(VERSION) > VERSION
+	@git add .
+	@git commit -m "Bumping to $(VERSION)"
+	@git push origin head
--- rv.rb
+++ rv.rb
@@ -1,10 +1,10 @@
 class Rv < Formula
-  @version = "v0.0.3"
+  @version = File.read("VERSION").chomp
 
   desc "AWS node resolver"
   homepage "https://github.com/ebenoist/rv"
   url "https://github.com/ebenoist/rv/releases/download/#{@version}/binaries.tar.gz"
-  sha256 "2c6990604258ebfcaa405b45d611fd37b907de700534ba5ef82ad1f6a189a537"
+  sha256 File.read("SHA").chomp
   version @version
 
   def install
