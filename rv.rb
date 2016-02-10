b36efc1da73d10ae3b43c43a418eefda59bd9413
Erik Benoist <ebenoist@gmail.com>
2016-02-10 07:43:23 -0600

    Fixes path issues

--- rv.rb
+++ rv.rb
@@ -1,10 +1,10 @@
 class Rv < Formula
-  @version = File.read("VERSION").chomp
+  @version = File.read(File.expand_path("../VERSION", __FILE__)).chomp
 
   desc "AWS node resolver"
   homepage "https://github.com/ebenoist/rv"
   url "https://github.com/ebenoist/rv/releases/download/#{@version}/binaries.tar.gz"
-  sha256 File.read("SHA").chomp
+  sha256 File.read(File.expand_path("../SHA", __FILE__)).chomp
   version @version
 
   def install
